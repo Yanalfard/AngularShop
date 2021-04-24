95b3d985b5ce9952a4c64d300efee1451ac3697c
Lemi <Lemi@DESKTOP-SDD69VQ>
2021-04-24 14:36:22 +0430

    Pages Created, Routes drawn, Awaiting componentization

--- Client/ShopClient/src/app/app-routing.module.ts
+++ Client/ShopClient/src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogViewComponent } from './pages/blog-view/blog-view.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FaqComponent } from './pages/faq/faq.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductComponent } from './pages/product/product.component';
+import { RulesComponent } from './pages/rules/rules.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AddressComponent } from './pages/user/address/address.component';
+import { CartComponent } from './pages/user/cart/cart.component';
+import { ChangePasswordComponent } from './pages/user/change-password/change-password.component';
+import { CompareComponent } from './pages/user/compare/compare.component';
+import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
+import { OrderCompletedComponent } from './pages/user/order-completed/order-completed.component';
+import { OrdersComponent } from './pages/user/orders/orders.component';
+import { ProfileComponent } from './pages/user/profile/profile.component';
+import { UserLayoutComponent } from './pages/user/user-layout/user-layout.component';
+import { WishlistComponent } from './pages/user/wishlist/wishlist.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    // ROOT
+    path: '', component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'rules', component: RulesComponent },
+      { path: 'faq', component: FaqComponent },
+      { path: 'blog', component: BlogsComponent },
+      { path: 'blog/:id', component: BlogViewComponent },
+      { path: 'product', redirectTo: 'search' },
+      { path: 'product/:id', component: ProductComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'compare', component: CompareComponent },
+      { path: 'wishlist', component: WishlistComponent },
+      { path: 'login', component: LoginComponent },
+      {
+        path: 'user', component: UserLayoutComponent,
+        children: [
+          { path: '', component: DashboardComponent },
+          { path: 'dashboard', redirectTo: '' },
+          { path: 'orders', component: OrdersComponent },
+          { path: 'profile', component: ProfileComponent },
+          { path: 'address', component: AddressComponent },
+          { path: 'changepassword', component: ChangePasswordComponent },
+          { path: 'orderCompleted', component: OrderCompletedComponent },
+        ]
+      },
+
+    ]
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
