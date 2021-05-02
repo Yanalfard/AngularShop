641b51d435827befaad9ee94ad46929e09f5072c
nazila bagheri <nazila.bagheri93@gmail.com>
2021-05-02 16:28:41 +0430

    components: blog-comment-info and in blog sidebar: archive and shop banner and tag

---
+++ Client/ShopClient/src/app/components/blog-sidebar/tag/tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss']
+})
+export class TagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
